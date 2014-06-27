336b526f36f51617d7649621337697fc4964b312
Artem Morozov - laptop <frost.artem@gmail.com>
2014-06-27 17:09:54 +0300

    push before merge

--- components/com_phase/views/client/tmpl/show_total_repo.php
+++ components/com_phase/views/client/tmpl/show_total_repo.php
@@ -129,3 +129,142 @@ foreach ($list[symptomList] as $value)
 ?>
 </ul>
 </div>
+
+
+<div class='contentheading'>Medical preparations Tracking</div>
+<div class='tabContainer2' style="background-color:#E1FFE3">
+    
+<?php
+
+
+$medtrack_symptoms = $last_list[drug][name];
+$medtrack_status =  $last_list[drug][status];
+
+
+if(count($medtrack_status) > 0){
+for($i = 0; count($medtrack_status) > $i; $i++)
+{
+    if($medtrack_status[$i] == "finished")
+    {
+        $medtrack_result[] = $medtrack_symptoms[$i];
+    }
+}
+}
+
+$medtrack_all = count($medtrack_symptoms);
+$medtrack_finish = count($medtrack_result);
+if($medtrack_finish == $all)
+{
+    echo "<div class='contentheading'> CONGRATULATIONS all the medical preparations FINISHED !</div>";
+}
+elseif($medtrack_finish > 0)
+{
+    echo "<div class='contentheading'>CONGRATULATIONS ".$medtrack_finish." out of ".$medtrack_all." symptoms FINISHED !</div>";
+}
+else{
+    echo "<div class='contentheading'>NO ONE Medical preparations IS FINISHID</div>";
+}
+?>
+<table>
+<?php
+if($list[medtrackList] && $list[medtrackList][0] !== "" && $list[medtrackList][name][0] !== "" && $result !== null){
+foreach ($list[medtrackList] as $value)
+{
+    if(in_array($value[id], $medtrack_result))
+    {
+    ?>
+        <tr><?="<td>".$value[name]."</td><td> - FINISHED ! </td>"?></tr>
+    <?php
+    }
+}
+}
+?>
+</table>
+</div>
+
+
+<div class='contentheading'>Diseases Tracking</div>
+<div class='tabContainer2' style="background-color:#E1FFE3">
+    
+<?php
+
+
+    $diseases_symptoms = $last_list[diseases][name];
+    $diseases_status =  $last_list[diseases][status];
+
+    
+
+if(count($diseases_status) > 0){
+for($i = 0; count($diseases_status) > $i; $i++)
+{
+    if($diseases_status[$i] == "finished")
+    {
+        $diseases_result[] = $diseases_symptoms[$i];
+    }
+}
+}
+
+$diseases_all = count($symptoms);
+$diseases_finish = count($result);
+if($diseases_finish == $diseases_all)
+{
+    echo "<div class='contentheading'> CONGRATULATIONS all diseases  FINISHED !</div>";
+}
+elseif($diseases_finish > 0)
+{
+    echo "<div class='contentheading'>CONGRATULATIONS ".$diseases_finish." out of ".$diseases_all." symptoms FINISHED !</div>";
+}
+else{
+    echo "<div class='contentheading'>NO ONE Medical preparations IS FINISHID</div>";
+}
+?>
+<table>
+<?php
+if($list[diseasesList] && $list[diseasesList][0] !== "" && $list[diseasesList][name][0] !== "" && $result !== null){
+foreach ($list[diseasesList] as $value)
+{
+    if(in_array($value[id], $diseases_result))
+    {
+    ?>
+        <tr><?="<td>".$value[name]."</td><td> - FINISHED ! </td>"?></tr>
+    <?php
+    }
+}
+}
+?>
+</table>
+</div>
+
+<div class='contentheading'>Photo History</div>
+<div class='tabContainer2' style="background-color:#E1FFE3">
+
+        <div class='contentheading'><?="Intake photo - ".$gols[photo][date]?></div>
+    <div class='contentheading'><?="Weight - ".$gols[body][val][0] ?></div>
+    <div class='contentheading'><?="Fat - ".$gols[body][val][1]." %" ?></div>
+    <div class='contentheading'><?="PH - ".$gols[body][val][2] ?></div>
+        <?= "  <div style='font-size:15px;color:#008;'><img src=\"".JURI::root().'uploads_jtpl/phase_details/'.$gols[photo][val][0]."\" width=\"200\" height=\"350\">";?>
+        <?= "  <img src=\"".JURI::root().'uploads_jtpl/phase_details/'.$gols[photo][val][1]."\" width=\"200\" height=\"350\"></div>";?>
+
+<?php
+
+if($content){
+foreach ($content as $value)
+{
+    
+    if($value[date][val] !== null && $value[body][val][0] !== "" && $value[body][val][1] !== "" && $value[body][val][2] !== ""){
+    ?>
+    <div class='tabContainer2' style="background-color:#E1FFE3">
+        <div class='contentheading'><?=$value[date][val]."<br>"?></div>
+        <div class='contentheading'><?="Weight - ".$value[body][val][0] ?></div>
+        <div class='contentheading'><?="Fat - ".$value[body][val][1]." %" ?></div>
+        <div class='contentheading'><?="PH - ".$value[body][val][2] ?></div>
+        <?= "  <div style='font-size:15px;color:#008;'><img src=\"".JURI::root().'uploads_jtpl/phase_details/'.$value[photo][0]."\" width=\"170\" height=\"320\">";?>
+        <?= "  <img src=\"".JURI::root().'uploads_jtpl/phase_details/'.$value[photo][1]."\" width=\"170\" height=\"320\"></div>";?>
+    </div>
+<?php
+}
+}
+}
+
+?>
+</div>
